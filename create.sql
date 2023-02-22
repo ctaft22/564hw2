a616e9b16b1d45e75bca064b445e6b7bab83699f
tomin <tominthomas63@gmail.com>
2023-02-21 20:01:05 -0600

    more load studd

--- create.sql
+++ create.sql
@@ -1,27 +1,28 @@
--- drop table if exists Items
-drop table if exists categories;
-drop table if exists Users
--- drop table if exists Bids
+drop TABLE if exists Items;
+drop TABLE if exists Categories;
+drop TABLE if exists Users;
+drop TABLE if exists Bids;
 
--- create table Items(
---     itemID int,
---     itemName Char(max),
---     currently Char(max),
---     buyPrice Float,
---     numBid int, 
---     startDate Datetime,
---     endDate Datetime,
---     description Char(max),
---     sellerID int
--- );
+CREATE TABLE Items(
+    itemID INTEGER PRIMARY KEY,
+    itemName TEXT,
+    currently TEXT,
+    buyPrice FLOAT,
+    firstBid FLOAT,
+    numBid INTEGER, 
+    startDate TEXT,
+    endDate TEXT,
+    description TEXT,
+    sellerID INTEGER
+);
 
-CREATE TABLE categories (
-    item_ID INTEGER PRIMARY KEY, 
+CREATE TABLE Categories (
+    item_ID INTEGER, 
     category TEXT,
 );
 
-create table Users(
-     userID TEXT,
+CREATE TABLE Users(
+     userID TEXT PRIMARY KEY,
      rating INTEGER, 
      location TEXT,
      country TEXT,
